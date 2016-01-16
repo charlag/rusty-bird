5d388381d1f36b4bd3182c3b41ecc030716e07df
Ivan Kupalov <ivan.kupalov@icloud.com>
2016-01-17 01:32:24 +0300

    Fixed getting request token The problem was using consumer_key instaead of consumer_secret as HMAC key.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#![allow(unused)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+
 extern crate hyper;
 extern crate rustc_serialize;
 extern crate rand;
@@ -7,6 +11,7 @@ extern crate crypto;
 
 use std::io::Read;
 use std::io;
+use std::collections::HashMap;
 
 use hyper::Client;
 use hyper::header::Connection;
@@ -17,36 +22,39 @@ use rustc_serialize::base64::{ToBase64, STANDARD};
 use rustc_serialize::json::Json;
 
 use url::percent_encoding;
-use url::percent_encoding::percent_encode;
 
 use crypto::{hmac, sha1};
 use crypto::mac::Mac;
+use crypto::md5::Md5;
+use crypto::digest::Digest;
+
+use std::ascii::AsciiExt;
+use rand::Rng;
+
 
 fn collect_params(consumer_key: &str, nonce: &str, timestamp: &str) -> String {
-    format!("oauth_consumer_key={}&oauth_nonce={}&oauth_signature_method=HMAC-SHA1&&oauth_timestamp={}&oauth_version=1.0",
+    format!("oauth_callback=oob&oauth_consumer_key={}&oauth_nonce={}&oauth_signature_method=HMAC-SHA1&oauth_timestamp={}&oauth_version=1.0",
                                     consumer_key, nonce, timestamp)
 }
 
 // Generating random BASE64-encoded string from 32 bytes of random data
 fn generate_nonce() -> String {
-    let mut arr: [u8; 32] = [0; 32];
-    for i in 0..32 {
-        arr[i] = rand::random::<u8>();
-    }
-    arr.to_base64(base64::STANDARD)
+    rand::thread_rng().gen_ascii_chars()
+        .take(42).collect()
 }
 
 fn auth() {
     let url = "https://api.twitter.com/oauth/request_token";
     let consumer_key = "";
+    let consumer_secret = "";
     let nonce = generate_nonce();
     let timestamp = time::get_time().sec.to_string();
     let mut collected_params = collect_params(consumer_key, &nonce, &timestamp);
-    let params_for_encoding = "POST&".to_string() + &percent_encode(url.as_bytes(), percent_encoding::FORM_URLENCODED_ENCODE_SET);
-    let percent_encoded_params = percent_encode(collected_params.as_bytes(),
+    let params_for_encoding = "POST&".to_string() + &percent_encoding::utf8_percent_encode(url, percent_encoding::FORM_URLENCODED_ENCODE_SET);
+    let percent_encoded_params = percent_encoding::utf8_percent_encode(&collected_params,
                                                         percent_encoding::FORM_URLENCODED_ENCODE_SET);
     let sign_base_string = params_for_encoding + "&" + &percent_encoded_params;
-    let signing_key = consumer_key.to_string() + &"&";
+    let signing_key = consumer_secret.to_string() + &"&";
     let mut hm = hmac::Hmac::new(sha1::Sha1::new(), signing_key.as_bytes());
     hm.input(sign_base_string.as_bytes());
     let size = hm.output_bytes();
@@ -54,20 +62,13 @@ fn auth() {
     let mut sign_slice = &mut sign_vec;
     hm.raw_result(&mut sign_slice);
     let sign = sign_slice.to_base64(base64::STANDARD);
-    //println!("{}", sign_base_string);
-    let sign_encoded = percent_encode(sign.as_bytes(),
+    let sign_encoded = percent_encoding::utf8_percent_encode(&sign,
                                         percent_encoding::FORM_URLENCODED_ENCODE_SET);
-    let nonce_encoded = percent_encode(nonce.as_bytes(),
-                                        percent_encoding::FORM_URLENCODED_ENCODE_SET);
-    /*
-    println!("{}", sign_encoded);
-    println!("{}", nonce_encoded);
-    println!("{}", timestamp);
-    */
+
     let client = Client::new();
     let mut headers = Headers::new();
-    let auth_string = format!("OAuth oauth_consumer_key=\"{}\", oauth_nonce=\"{}\", oauth_signature=\"{}\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"{}\", oauth_version=\"1.0\"",
-                        consumer_key, nonce_encoded, sign_encoded, timestamp);
+    let auth_string = format!("OAuth oauth_callback=oob, oauth_consumer_key=\"{}\", oauth_nonce=\"{}\", oauth_signature=\"{}\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"{}\", oauth_version=\"1.0\"",
+                        consumer_key, nonce, sign_encoded, timestamp);
     let mut auth_vec = Vec::new();
     auth_vec.extend(auth_string.as_bytes().iter());
     headers.set_raw("Authorization", vec![auth_vec]);
@@ -85,14 +86,20 @@ fn auth() {
         .expect("Response reading error");
 
     println!("{}", response);
-
+    let mut response_map = HashMap::<&str, &str>::new();
+    for pair in response.split('&') {
+        let mut t = pair.split('=');
+        response_map.insert(t.next().unwrap(), t.next().unwrap());
+    }
+     println!("Token: {}", response_map.get("oauth_token").unwrap());
+     println!("Token secret: {}", response_map.get("oauth_token_secret").unwrap());
+    println!("https://api.twitter.com/oauth/authorize?oauth_token={}",
+            response_map.get("oauth_token").unwrap());
 }
 
 fn main() {
-    if 2 != 3 {
-        auth();
-        return;
-    }
+    auth();
+    return;
 
     let url = "https://api.twitter.com/oauth2/token";
     //let url = "http://requestb.in/1fbwty61";
